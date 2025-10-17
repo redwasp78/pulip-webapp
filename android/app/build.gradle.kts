a014c07ed958142e61878ec2efcd00ef6803727d
redwasp <redwasp@pulip.com>
2025-10-17 14:09:23 +0900

    🔧 flutter_spinkit 및 Android NDK 호환성 문제 해결
    - flutter_spinkit 5.2.0 → 5.1.0 다운그레이드 (withValues 오류 해결)
    - Android NDK 버전 27.0.12077973로 업데이트
    - Flutter 3.24.0 호환성 문제 해결

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 android {
     namespace = "kr.pe.kk.pulip_webapp"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
